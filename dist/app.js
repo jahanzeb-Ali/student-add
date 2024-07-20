ce33f294119c0c5514717aa6766c39eda46d9f5a
Jahanzeb <jahanzebsoomro543@gmail.com>
2024-07-20 11:57:53 +0500

    complete

--- dist/app.js
+++ dist/app.js
@@ -1,48 +1,65 @@
-const studentName = document.getElementById("student-name")
-const studentUserName = document.getElementById("student-userName")
-const studentEmail = document.getElementById("student-email")
-const addStudentBtn = document.getElementById("addBtn")
-const tableDisplay = document.getElementById('table-display')
-let studentList = [];
-let count = 0;
-let rollNuber = 1800;
-addStudentBtn.addEventListener("click", () =>{
-   let name = studentName.value.trim();
-   let userName = studentUserName.value.trim();
-   let email = studentEmail.value.trim();
-   if(name !== "" && userName !=""&& email !== "" ){
-               let obj = {
-                "name": name,
-                "userName": userName,
-                "email":email,
-                "roll": ++rollNuber
-            }
-            studentList.push(obj)
-            localStorage.setItem(`student${++count}`, JSON.stringify(obj))
-            tableMaker(obj)
-            resetValueOfInp()
-        }else{
-            alert("Fill text fields")
-        }
-        
-})
-function tableMaker(student){
-    tableDisplay.innerHTML +=`<tr>
-         <th class=" p-3">${++count}</th>
-        <td class=" p-3">${student['name']}</td>
-        <td class=" p-3">${student['userName']}</td>
-        <td class=" p-3">${student['email']}</td>
-        <td class=" p-3">${student['roll']} <button class="bg-blue-700 rounded-md p-2 text-white" onclick="removeStudent(this)">Remove</button></td>
-    </tr>`
-}
-function resetValueOfInp(){
-    studentName.value = ""
-    studentUserName.value = ""
-    studentEmail.value = ""
+
+const studentName = document.getElementById("student-name");
+const studentUserName = document.getElementById("student-userName");
+const studentEmail = document.getElementById("student-email");
+const addStudentBtn = document.getElementById("addBtn");
+const tableDisplay = document.getElementById('table-display');
+let studentList = JSON.parse(localStorage.getItem('studentdata')) ?? [];
+let num = 0;
+
+addStudentBtn.addEventListener("click", () => {
+    let rollNumber = Math.round(Math.random() * 10000) + 1000;
+    let name = studentName.value.trim();
+    let userName = studentUserName.value.trim();
+    let email = studentEmail.value.trim();
+
+    if (name !== "" && userName !== "" && email !== "") {
+        let obj = {
+            "name": name,
+            "userName": userName,
+            "email": email,
+            "roll": rollNumber
+        };
+        studentList.push(obj);
+        localStorage.setItem('studentdata', JSON.stringify(studentList));
+        ++num;
+        tableMaker(obj, num);
+        resetValueOfInp();
+    } else {
+        alert("Fill text fields");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", makeTable);
+
+function makeTable() {
+    num = 0;
+    for (let item of studentList) {
+        ++num;
+        tableMaker(item, num);
+    }
 }
 
+function tableMaker(student, num) {
+    tableDisplay.innerHTML += `<tr>
+        <th class="p-3">${num}</th>
+        <td class="p-3">${student['name']}</td>
+        <td class="p-3">${student['userName']}</td>
+        <td class="p-3">${student['email']}</td>
+        <td class="p-3">${student['roll']} <button class="bg-blue-700 rounded-md p-2 text-white" onclick="removeStudent(this,${num})">Remove</button></td>
+    </tr>`;
+}
 
-function removeStudent(item){
-    let row = (item.parentNode.parentNode)
-    row.parentNode.removeChild(row)
+function resetValueOfInp() {
+    studentName.value = "";
+    studentUserName.value = "";
+    studentEmail.value = "";
+}
+
+function removeStudent(item, index) {
+    let row = item.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    studentList.splice(index - 1, 1);
+    localStorage.setItem('studentdata', JSON.stringify(studentList));
+    location.reload();
 }
