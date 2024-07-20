0ae9df70c93256583200ae8e4ce6bbef16387b09
Jahanzeb <jahanzebsoomro543@gmail.com>
2024-07-20 12:04:29 +0500

    complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["dist/*.{html,js,jsx,ts,tsx}"
+  content: ["*.{html,js,jsx,ts,tsx}","dist/*.{html,js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
